e0f8eac8f0d2a9562264758c61e8fbd7c9aac84f
uttamang <w.uttamang@gmail.com>
2017-05-04 12:22:56 +0200

    scanner and inout files

---
+++ Scanner.cpp
@@ -0,0 +1,356 @@
+// K7scan0.cpp 
+//
+
+
+#include "stdafx.h"
+#include <iostream>
+#pragma warning(disable:4786)
+#include <string>
+#include <map>
+using namespace std;
+
+#define	Getc(s)			getc(s)
+#define	Ungetc(c)		ungetc(c,IP_Input)
+
+
+/*
+ *	Lexical analyzer states.
+ */
+enum lexstate{L_START,L_INT,L_ELEMENT_NAME, L_BESCHREIBUNG, L_NODE1, L_NODE2};
+
+const int ELEMENT_NAME=4;
+const int INTEGER1=5;
+const int BESCHREIBUNG = 500;
+const int TOKENSTART=300;
+const int TYPE = 10;
+const int NODE1 = 7;
+const int NODE2 = 8;
+
+class CParser
+{
+public:
+
+	string yytext;								//input buffer
+	struct tyylval{								//value return
+		string s;								//structure
+		int i;
+	}yylval;
+	FILE *IP_Input;								//Input File
+	FILE *IP_Error;								//Error Output
+	FILE *IP_List;								//List Output
+	int  IP_LineNumber;							//Line counter
+	map<string,int> IP_Token_table;				//Tokendefinitions
+	map<int,string> IP_revToken_table;			//reverse Tokendefinitions
+
+
+	int CParser::yylex();						//lexial analyser
+	void CParser::yyerror(char *ers);			//error reporter
+	int CParser::IP_MatchToken(string &tok);	//checks the token
+	void CParser::InitParse(FILE *inp,FILE *err,FILE *lst);
+	int	CParser::yyparse();						//parser
+	void CParser::pr_tokentable();				//test output for tokens
+	void CParser::IP_init_token_table();		//loads the tokens
+	void CParser::Load_tokenentry(string str,int index);//load one token
+	CParser(){IP_LineNumber = 1;};	//Constructor
+};
+//------------------------------------------------------------------------
+
+void CParser::Load_tokenentry(string str,int index)
+{
+	IP_Token_table[str]=index;
+	IP_revToken_table[index]=str;
+}
+void CParser::IP_init_token_table()
+{
+	Load_tokenentry("ELEMENT_NAME",4);
+	Load_tokenentry("INTEGER1",5);
+	Load_tokenentry("NODE1", 7);
+	Load_tokenentry("NODE2", 8);
+	Load_tokenentry("ELEMENT", 9);
+	int type = TYPE;
+	Load_tokenentry("R", type++);
+	Load_tokenentry("C", type++);
+	Load_tokenentry("L", type++);
+
+	int i = BESCHREIBUNG;
+	Load_tokenentry("NETS", i++);
+	Load_tokenentry("IN", i++);
+	Load_tokenentry("OUT", i++);
+	Load_tokenentry("CMN", i++);
+	Load_tokenentry("INTERNAL", i++);
+
+	int ii=TOKENSTART;
+	Load_tokenentry("(",ii++);
+	Load_tokenentry(")",ii++);
+	Load_tokenentry(":",ii++);
+	Load_tokenentry(";", ii++);
+}
+//------------------------------------------------------------------------
+
+void CParser::pr_tokentable()
+{
+	
+	typedef map<string,int>::const_iterator CI;
+	const char* buf;
+
+	printf( "Symbol Table ---------------------------------------------\n");
+
+	for(CI p=IP_Token_table.begin(); p!=IP_Token_table.end(); ++p){
+		buf = p->first.c_str();
+		printf(" key:%s", buf);
+		printf(" val:%d\n", p->second);;
+	}
+}
+//------------------------------------------------------------------------
+
+int	CParser::yyparse()
+{
+	int tok;
+	/*
+	*	Go parse things!
+	*/
+	while ((tok=yylex())!=0){
+		printf("%d ",tok);
+		if (tok == INTEGER1)
+			printf("%s %d ", IP_revToken_table[tok].c_str(), yylval.i);
+		else
+			if (tok == ELEMENT_NAME)
+				printf("%s %s ", IP_revToken_table[tok].c_str(), yylval.s.c_str());
+			else
+				if (tok >= BESCHREIBUNG)
+					printf(" BESCHREIBUNG : %s %s", IP_revToken_table[tok].c_str(), yylval.s.c_str());
+				else
+					if (tok >= TOKENSTART)
+						printf("%s ", IP_revToken_table[tok].c_str());
+					else
+						if (tok >= TYPE && tok <= 13)
+							printf("TYPE: %s ", IP_revToken_table[tok].c_str());
+						else
+							if (tok == NODE1)
+								printf("%s : %s", IP_revToken_table[tok].c_str(), yylval.s.c_str());
+							else
+								if (tok == NODE2)
+									printf("%s : %s", IP_revToken_table[tok].c_str(), yylval.s.c_str());
+								else
+									printf("%c ",tok);
+		printf("\n");
+	}
+	return 0;
+
+}
+//------------------------------------------------------------------------
+
+/*
+ *	Parse Initfile:
+ *
+ *	  This builds the context tree and then calls the real parser.
+ *	It is passed two file streams, the first is where the input comes
+ *	from; the second is where error messages get printed.
+ */
+void CParser::InitParse(FILE *inp,FILE *err,FILE *lst)
+
+{
+
+	/*
+	*	Set up the file state to something useful.
+	*/
+	IP_Input = inp;
+	IP_Error = err;
+	IP_List  = lst;
+
+	IP_LineNumber = 1;
+	/*
+	*	Define both the enabled token and keyword strings.
+	*/
+	IP_init_token_table();
+}
+//------------------------------------------------------------------------
+
+/*
+ *	yyerror:
+ *
+ *	  Standard error reporter, it prints out the passed string
+ *	preceeded by the current filename and line number.
+ */
+void CParser::yyerror(char *ers)
+
+{
+  fprintf(IP_Error,"line %d: %s\n",IP_LineNumber,ers);
+}
+//------------------------------------------------------------------------
+
+int CParser::IP_MatchToken(string &tok)
+{
+	int retval;
+	if(	IP_Token_table.find(tok) != IP_Token_table.end()){
+		retval = (IP_Token_table[tok]);
+	}else{
+		retval = (0);
+	}
+	return retval;
+} 
+
+//------------------------------------------------------------------------
+
+/*
+ *	yylex:
+ *
+ */
+int CParser::yylex()
+{
+	//Locals
+	int c;
+	size_t pos;
+	string temp_text;
+	lexstate s;
+	/*
+	*	Keep on sucking up characters until we find something which
+	*	explicitly forces us out of this function.
+	*/
+	for (s = L_START,yytext = ""; 1;){
+		if (IP_LineNumber == 1)
+			s = L_BESCHREIBUNG;
+
+		c = Getc(IP_Input);
+		yytext = yytext + (char)c;
+		switch (s){
+		  //Starting state, look for something resembling a token.
+			case L_START:
+				if (isdigit(c)){
+				  s = L_INT;
+				}else if (isalpha(c) ){
+						s = L_ELEMENT_NAME;
+				}else if (isspace(c)){
+							if (c == '\n'){
+								IP_LineNumber += 1;
+							}
+							yytext = "";
+				}else if (c == EOF){
+							return ('\0');
+				}
+				else if (c == '(')
+					s = L_NODE1;
+				else if (c == ',')
+					s = L_NODE2;
+				else{						
+						return (c);
+				}
+			break;
+
+		  /*
+		   *	Suck up the integer digits.
+		   */
+			case L_INT:
+				if (isdigit(c)){
+				  break;
+				}else {
+					Ungetc(c);
+					yylval.s = yytext.substr(0,yytext.size()-1);
+					yylval.i = atoi(yylval.s.c_str());
+					return (INTEGER1);
+				}
+			break;
+
+		  /*
+		   *	Grab an identifier, see if the current context enables
+		   *	it with a specific token value.
+		   */
+			
+			case L_ELEMENT_NAME:
+			   if (isalpha(c) || isdigit(c) || c == '_')
+				  break;
+				Ungetc(c);
+				yytext = yytext.substr(0,yytext.size()-1);
+				if (yytext == "NETS")
+				{
+					s = L_BESCHREIBUNG;
+					yytext = "";
+					break;
+				}
+				
+				yylval.s = yytext;
+				if (c = IP_MatchToken(yytext)){
+					return (c);
+				}else{
+					return (ELEMENT_NAME);
+				}
+			
+			case L_BESCHREIBUNG:
+				if (isalpha(c) || c == ',')
+					break;
+				yytext = yytext.substr(0, yytext.size());
+				if (yytext.find(':', 0)!= string::npos)
+				{
+					temp_text = yytext.substr(0, yytext.size()-1);
+					yytext = "";
+					break;
+				}
+				else
+					if (yytext.find(';', 0) != string::npos)
+						{
+							yylval.s = temp_text;
+							yytext = yytext.substr(0,yytext.size()-1);
+						}
+						else
+
+							if (yytext == "NETS")
+							{
+								s = L_BESCHREIBUNG;
+								yytext = "";
+								break;
+							}
+								else
+									if (isspace(c)) 
+									{
+										if (c == '\n') {
+											IP_LineNumber += 1;
+										}
+										yytext = "";
+										s = L_START;
+										break;
+									}
+				return (IP_MatchToken(yytext));
+
+			case L_NODE1:
+				if (isalpha(c))
+					break;
+				Ungetc(c);
+				yytext = yytext.substr(1, yytext.size() - 2);
+				yylval.s = yytext;
+				return (NODE1);
+
+			case L_NODE2:
+				if (isalpha(c))
+					break;
+				Ungetc(c);
+				yytext = yytext.substr(1, yytext.size() - 2);
+				yylval.s = yytext;
+				return (NODE2);
+
+			default: printf("***Fatal Error*** Wrong case label in yylex\n");
+		}
+	}
+}
+//------------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+	FILE *inf;
+	char fistr[100]="in1.txt";
+	                     // change from gets(fistr)
+	inf = fopen(fistr,"r");
+	if(inf==NULL){
+		printf("Cannot open input file %s\n",fistr);
+		return 0;
+	}
+	CParser obj;
+	obj.InitParse(inf,stderr,stdout);
+//	obj.pr_tokentable();
+	obj.yyparse();
+	printf("finished");
+	char c; cin>>c;
+
+	return 0;
+}
+
+
