2993e84a1fa77b45db6982d559505261d28063b7
uttamang <w.uttamang@gmail.com>
2017-05-04 12:24:41 +0200

    Add files via upload

---
+++ StdAfx.h
@@ -0,0 +1,24 @@
+// stdafx.h : Include-Datei für Standard-System-Include-Dateien,
+//  oder projektspezifische Include-Dateien, die häufig benutzt, aber
+//      in unregelmäßigen Abständen geändert werden.
+//
+
+#if !defined(AFX_STDAFX_H__232122CC_85E8_11D7_94C8_00E0987DE58D__INCLUDED_)
+#define AFX_STDAFX_H__232122CC_85E8_11D7_94C8_00E0987DE58D__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Selten benutzte Teile der Windows-Header nicht einbinden
+#define _CRT_SECURE_NO_WARNINGS //V8
+#include <stdio.h>
+#include <cstdio>
+#include <memory.h>				//V8
+
+// ZU ERLEDIGEN: Verweisen Sie hier auf zusätzliche Header-Dateien, die Ihr Programm benötigt
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ fügt zusätzliche Deklarationen unmittelbar vor der vorherigen Zeile ein.
+
+#endif // !defined(AFX_STDAFX_H__232122CC_85E8_11D7_94C8_00E0987DE58D__INCLUDED_)
